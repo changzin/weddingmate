dd923020df1500f4c2f4696e74ae0c15462f6da3
KANGMOONJEONG <image102@naver.com>
2024-06-18 08:50:36 +0900

    라우터

--- frontend/weddingmate/src/App.vue
+++ frontend/weddingmate/src/App.vue
@@ -1,9 +1,12 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/productdetail">ProductDetail</router-link> |
+    </nav>
+    <router-view/>
+  </div>
 </template>
 
 <style>
