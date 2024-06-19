c04918b15dc1aa659593b68f668a49ae1d42ddea
KANGMOONJEONG <image102@naver.com>
2024-06-19 17:22:01 +0900

    index.js

--- frontend/weddingmate/src/router/index.js
+++ frontend/weddingmate/src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import ProductDetail from '../views/ProductDetail.vue'
+import ProductDetail from '../views/product/ProductDetail.vue'
+import ProductDetailList from '../views/product/ProductDetailList.vue'
 import QnAWrite from '../views/QnA/QnAWrite.vue'
 import QnAModify from '../views/QnA/QnAModify.vue'
 import QnADetail from '../views/QnA/QnADetail.vue'
 import QnAList from '../views/QnA/QnAList.vue'
+import ReviewList from '../views/review/ReviewList.vue'
 
 import UserLogin from '../views/nouser/UserLogin.vue'
 const routes = [
@@ -25,6 +27,11 @@ const routes = [
     path: '/productdetail',
     name: 'productdetail',
     component: ProductDetail
+  },
+  {
+    path: '/productdetaillist',
+    name: 'productdetaillist',
+    component: ProductDetailList
   },
   {
     path: '/qnawrite',
@@ -51,6 +58,11 @@ const routes = [
     path: '/userlogin',
     name: 'userlogin',
     component: UserLogin
+  },
+  {
+    path: '/reviewlist',
+    name: 'reviewlist',
+    component: ReviewList
   },
 ]
 
