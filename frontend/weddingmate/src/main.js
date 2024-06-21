34dec8a3ce17bf7f6e90a78b36b7aea41ff9539f
KANGMOONJEONG <image102@naver.com>
2024-06-21 15:29:04 +0900

    aaa

--- frontend/weddingmate/src/main.js
+++ frontend/weddingmate/src/main.js
@@ -4,7 +4,16 @@ import router from './router'
 import store from './util/store'
 import mixins from './util/mixins'
 
+import EventCalendar from './components/EventCalendar.vue';
+
+import VCalendar from 'v-calendar'; // v-calendar 플러그인 임포트
+import 'v-calendar/dist/style.css'; // v-calendar 스타일 임포트
+
 const app = createApp(App);
 app.use(store);
 app.mixin(mixins);
+
+app.component('EventCalendar', EventCalendar);
+app.use(VCalendar, {});
 app.use(router).mount('#app');
+
