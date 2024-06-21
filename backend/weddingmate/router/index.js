2f7771ffed2860beee4fa8db71110479da310c1b
sgy0917 <sgy0917@naver.com>
2024-06-21 17:31:01 +0900

    order backend추가

--- backend/weddingmate/router/index.js
+++ backend/weddingmate/router/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const userRouter = require('./userRouter');
 const reviewRouter = require('./reviewRouter');
+const orderRouter = require('./orderRouter');
 const router = express.Router();
 
 // 여러 라우터 파일을 모아서 묶어주는 역할을 수행
 router.use("/user", userRouter);
 router.use("/review", reviewRouter);
-
+router.use("/order",orderRouter);
 
 module.exports = router;
